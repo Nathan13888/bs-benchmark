a5d2ba50d3aa1ef126b8183b3593a79cecbdd54e
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2023-06-27 18:49:53 -0400

    refactor Mkdir function
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- utils.go
+++ utils.go
@@ -1,7 +1,23 @@
 package main
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 func GetShortTime() string {
 	return time.Now().Format("20060102150405")
+}
+
+func Mkdir(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		// create directory
+		err := os.Mkdir("./outputs", 0755)
+
+		// TODO: check if error is "file exists"
+		return err
+	}
+
+	// path already exists
+	return nil
 }
